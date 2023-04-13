ae2825706cba1fdb25984f72b4a0fb72c9b87fbb
carlodavid225 <dcarlo318@gmail.com>
2023-04-13 16:58:43 +0800

    updated

--- json.php
+++ json.php
@@ -3,7 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 $perfumes = array(
     array(
-        "barcode_id" => "9297427462",
         "brand" => "Penshoppe",
         "name" => "Hustle Night Eau de Toilette",
         "color" => "Black",
@@ -12,7 +11,6 @@ $perfumes = array(
         "ingredient" => "Sodium Tallowate Sodium Cocoate Fragrance Triclocarbon."
     ),
     array(
-        "barcode_id" => "9287482364",
         "brand" => "Penshoppe",
         "name" => "Lovestory Eau De Toilette",
         "color" => "Gray",
@@ -21,7 +19,6 @@ $perfumes = array(
         "ingredient" => "Black pepper Cardamom Mint Vanilla linger Lavender."
     ),
     array(
-        "barcode_id" => "4665874656",
         "brand" => "Versace",
         "name" => "Versace eros",
         "color" => "Black",
@@ -30,7 +27,6 @@ $perfumes = array(
         "ingredient" => "Alcohol Denat. (Sd Alcohol 39 C) Fragrance Coumarine Linalool."
     ),
     array(
-        "barcode_id" => "0329472984",
         "brand" => "Dior",
         "name" => "SAUVAGE EAU DE TOILETTE",
         "color" => "Blue",
@@ -39,7 +35,6 @@ $perfumes = array(
         "ingredient" => "Alcohol Aqua(water) Fragrance Imonene Linolool."
     ),
     array(
-        "barcode_id" => "-1831977313",
         "brand" => "Penshoppe",
         "name" => "Pumped Blue Eau De Toilette",
         "color" => "Blue",
